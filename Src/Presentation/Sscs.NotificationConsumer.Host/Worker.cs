1c4ead2b8ef4d4cea9752d3ea3ddcd17aed5a066
KungurtsevNI <KungurtsevNI@gmail.com>
2021-08-16 12:07:17 +0400

    Шина на базе RabbitMQ с ипользование EasyNETQ

--- Src/Presentation/Sscs.NotificationConsumer.Host/Worker.cs
+++ Src/Presentation/Sscs.NotificationConsumer.Host/Worker.cs
@@ -27,13 +27,18 @@ namespace Sscs.NotificationConsumer.Host
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation("Сервис начал работать");
             var subscriptionIdPrefix = _configuration.GetValue<string>("RabbitMQSubscriptionIdPrefix");
             var subscriber = new AutoSubscriber(_bus, subscriptionIdPrefix)
             {
                 GenerateSubscriptionId = c => AppDomain.CurrentDomain.FriendlyName + " " + c.ConcreteType.Name
             };
 
+            _logger.LogInformation("Настроили subscriber'ов");
+            
             await subscriber.SubscribeAsync(new []{ Assembly.GetExecutingAssembly() }, stoppingToken);
+            
+            _logger.LogInformation("Подписали найденных субскайребов");
         }
     }
 }
